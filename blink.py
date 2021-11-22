0478c8f72c7fe1b91595b7e5b536a3eee3b28430
Steve Rosenstein <serosenstein@gmail.com>
2021-11-21 23:15:00 -0700

    support emails,prowl.twilio sms

--- blink.py
+++ blink.py
@@ -2,6 +2,7 @@
 from blinkpy.blinkpy import Blink
 from blinkpy.auth import Auth
 import os
+import sys
 import time
 import datetime
 from blinkpy.helpers.util import json_load
@@ -9,23 +10,26 @@ from blinkpy.helpers.util import json_load
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
-
+import vardata
 pid = os.getpid()
 pid = str(pid)
 
-interval = 120
-counts = 360
-camera_name = 'aerocam'
-fontLocation = "/usr/lib/plexmediaserver/Resources/Fonts/DejaVuSans-Bold.ttf"
+interval = vardata.interval
+counts = vardata.counts
+camera_name = vardata.camera_name
+fontLocation = vardata.fontLocation
+dir = vardata.dir
+credSave = vardata.credSave
+
+send_prowls = vardata.send_prowls
+send_emails = vardata.send_emails
+send_twilio = vardata.send_twilio
 
 
 now = datetime.datetime.now()
-dir = './time-lapses/'
 date = now.strftime('%Y-%m-%d-%H')
 timestamp = now.strftime('%Y-%m-%d %H:%M:%S')
-#print(timestamp)
 date = str(date)
-credSave = "./blink.credsave"
 print(date)
 dirName = str(dir) + date + "." + pid
 print(dirName)
@@ -34,12 +38,12 @@ blink = Blink()
 if not os.path.exists(dirName):
   os.makedirs(dirName)
   print("Directory " , dirName ,  " Created ")
-else:    
+else:
   print("Directory " , dirName ,  " already exists")
 auth = Auth(json_load(credSave), no_prompt=True)
 blink.auth = auth
 blink.start()
-blink.save("./blink.credsave")
+blink.save(credSave)
 camera = blink.cameras[camera_name]
 for i in range (counts):
    camera.snap_picture()       # Take a new picture with the camera
@@ -54,8 +58,85 @@ for i in range (counts):
    font = ImageFont.truetype(font=fontLocation, size=36, index=0, encoding='', layout_engine=None)
    img = Image.open(fileName)
    I1 = ImageDraw.Draw(img)
-   #I1.text((36, 36), timestamp, fill=(255, 0, 0))
    I1.text((36, 36), timestamp, fill=(255, 0, 0), font=font)
    img.save(fileName)
+   print("number " + str_i + " out of " + str(counts))
    time.sleep(interval)
 os.system('ffmpeg -i ' + dirName + "/image%05'd'.jpg -r 60 -s 640x480 -vcodec libx264 -b 1000k " + dirName + "/" + date + '.output.mp4')
+if send_emails:                                               
+  import smtplib, ssl
+  sender_email = vardata.sender_email                         
+  smtp_password = vardata.smtp_password                       
+  receiver_email = vardata.receiver_email                     
+  smtp_server = vardata.smtp_server                           
+  smtp_port = vardata.smtp_port                               
+  if not [x for x in (sender_email,smtp_password,receiver_email,smtp_server,smtp_port) if x is None]:                                         
+    pass
+
+def SendTwilio(info):
+  from twilio.rest import Client
+  twilio_to = vardata.twilio_to 
+  twilio_from = vardata.twilio_from
+  twilio_sid = vardata.twilio_sid
+  twilio_token = vardata.twilio_token
+  if not [x for x in (twilio_to,twilio_sid,twilio_token) if x is None]:
+    pass
+  client = Client(twilio_sid, twilio_token)
+  message = client.messages \
+                .create(
+                     body=info,
+                     from_=twilio_from,
+                     to=twilio_to)
+def SendEmail(info):
+ # For guessing MIME type
+ import mimetypes
+
+ # Import the email modules we'll need
+ import email
+ import email.mime.application
+ from email.mime.multipart import MIMEMultipart
+
+ # Create a text/plain message
+ msg = email.mime.multipart.MIMEMultipart()
+
+ msg['Subject'] = 'TimeLapse Video'
+ msg['From'] = sender_email
+ msg['To'] = receiver_email
+
+
+ filename=dirName + "/" + date + '.output.mp4'
+ fp=open(filename,'rb')
+ att = email.mime.application.MIMEApplication(fp.read(),_subtype="video/mp4")
+ fp.close()
+ att.add_header('Content-Disposition','attachment',filename=filename)
+ msg.attach(att)
+ try:
+  context = ssl.create_default_context()
+  with smtplib.SMTP(smtp_server, smtp_port) as server:
+    server.ehlo()
+    server.starttls(context=context)
+    server.ehlo()
+    server.login(sender_email, smtp_password)
+    server.sendmail(sender_email, receiver_email, msg.as_string())
+  #debug comment
+  print("sending email to " + receiver_email + " with this info " + info)
+ except:
+  print("Error sending email: " + str(sys.exc_info()[0])) 
+  raise
+def SendProwl(info):
+ from pushno import PushNotification
+ apikey = vardata.apikey
+ pn = PushNotification(
+     "prowl", api_key=apikey, application="TimeLapse"
+ )
+ is_valid, res = pn.validate_user()
+ if is_valid:
+     pn.send(event="Timelapse Notification", description=info)
+ else:
+     print(res)
+if send_prowls:
+  SendProwl("Finished " +  dirName + "/" + date + '.output.mp4')
+if send_emails:
+  SendEmail("Finished " +  dirName + "/" + date + '.output.mp4')
+if send_twilio:
+  SendTwilio("Finished " +  dirName + "/" + date + '.output.mp4')
---
+++ example_vardata.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+#Rename this file to vardata.py after you modify it
+
+#prowl api key
+apikey="1234123412341234123412341091a12341cdee15"
+
+#smtp stuff
+sender_email="whatever@domain.com"
+smtp_password="YourSecretPassword"
+receiver_email="suckerwhogetsemails@domain.com"
+smtp_server="smtp.gmail.com"
+smtp_port="587"
+send_emails=True
+send_prowls=True
+#if needing twilio support: pip3 install twilio 
+send_twilio=True
+
+
+#Twilio SMS stuff
+twilio_to="+11234523450"
+twilio_from="+11234123420"
+twilio_sid="adflkjadaflkjaldflkjasdflkjadsfksb"
+twilio_token="asdlfjasdflkasdlfkjasdfrlslslsls"
+
+
+interval = 10
+counts = 2
+camera_name = 'aerocam'
+fontLocation = "NimbusMonoPS-Bold.t1"
+dir = './time-lapses/'
+credSave = "./blink.credsave"
